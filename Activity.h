42a8f65353d4d4b8bb6ab2788da09a21115d23c0
Mattia Bonanni <mattia.bonanni2@edu.unifi.it>
2024-06-27 19:52:42 +0200

    Adding methods to classes

--- Activity.h
+++ Activity.h
@@ -9,6 +9,17 @@ using namespace std;
 
 class Activity {
 public:
+    Activity(string description, time_t start, time_t end);
+
+    //getter and setter methods:
+    const string &getDescription() const;
+    void setDescription(const string &description);
+
+    time_t getStart() const;
+    void setStart(time_t start);
+
+    time_t getAnEnd() const;
+    void setAnEnd(time_t anEnd);
 
 private:
     string description;
--- Register.h
+++ Register.h
@@ -8,6 +8,16 @@
 using namespace std;
 
 class Register {
+public:
+    //default constructor
+    Register();
+
+    // Method to add an activity for a specific day
+    void addActivity(const std::string& day, const Activity activity);
+
+    // Method to print activities for each day
+    void printActivitiesByDay(string day) const;
+
 private:
     multimap<string, Activity> activitiesByDay;
 

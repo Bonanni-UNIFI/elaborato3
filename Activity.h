2979b88ac7be4b3b3ef4dab2561be0b63552e8a6
Mattia Bonanni <mattia.bonanni2@edu.unifi.it>
2024-06-27 19:49:03 +0200

    Adding attributes to classes

--- Activity.h
+++ Activity.h
@@ -1,9 +1,19 @@
 #ifndef ELABORATO3_ACTIVITY_H
 #define ELABORATO3_ACTIVITY_H
 
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
 
 class Activity {
+public:
 
+private:
+    string description;
+    time_t start;
+    time_t end;
 };
 
 
--- Register.h
+++ Register.h
@@ -1,8 +1,15 @@
 #ifndef ELABORATO3_REGISTER_H
 #define ELABORATO3_REGISTER_H
 
+#include "Activity.h"
+#include <map>
+#include <string>
+
+using namespace std;
 
 class Register {
+private:
+    multimap<string, Activity> activitiesByDay;
 
 };
 

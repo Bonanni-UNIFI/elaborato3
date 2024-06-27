69348484d19c509eed7dab452b0c34619b94f8a4
Mattia Bonanni <mattia.bonanni2@edu.unifi.it>
2024-06-27 20:15:15 +0200

    Defining methods without using graphical interface

--- Activity.cpp
+++ Activity.cpp
@@ -1,1 +1,12 @@
 #include "Activity.h"
+
+Activity::Activity(string description, time_t start, time_t end):description(description), start(start), end(end) {}
+
+const string &Activity::getDescription() const { return description; }
+void Activity::setDescription(const string &description) { Activity::description = description; }
+
+time_t Activity::getStart() const { return start; }
+void Activity::setStart(time_t start) { Activity::start = start; }
+
+time_t Activity::getAnEnd() const { return end; }
+void Activity::setAnEnd(time_t anEnd) { end = anEnd; }
--- Register.cpp
+++ Register.cpp
@@ -1,1 +1,18 @@
 #include "Register.h"
+
+Register::Register() = default;
+
+void Register::addActivity(const std::string& day, const Activity activity) {
+    activitiesByDay.insert(std::make_pair(day, activity));
+}
+
+// Method to print activities for each day
+void Register::printActivitiesByDay(string day) const{
+    auto range = activitiesByDay.equal_range(day);
+    std::cout << day << ":\n";
+    for (auto it = range.first; it != range.second; ++it) {
+        std::cout << "inizio - " << it->second.getStart() << endl;
+        std::cout << "fine - " << it->second.getAnEnd() << endl;
+        std::cout << "descrizione - " << it->second.getDescription() << endl;
+    }
+}

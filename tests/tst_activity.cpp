3fe4cb7742dc2939502c64b2eb8d68a374f6ab94
Mattia Bonanni <mattia.bonanni2@edu.unifi.it>
2024-07-15 11:01:50 +0200

    Defining methods in tst_activity.cpp

--- tests/tst_activity.cpp
+++ tests/tst_activity.cpp
@@ -2,11 +2,24 @@
 #include "Activity.h"
 
 TEST(ActivityTest, Constructor) {
+Activity activity("Meeting", QTime(9, 0), QTime(10, 0));
 
+EXPECT_EQ(activity.getDescription(), "Meeting");
+EXPECT_EQ(activity.getStart(), QTime(9, 0));
+EXPECT_EQ(activity.getAnEnd(), QTime(10, 0));
 }
 
 TEST(ActivityTest, SettersAndGetters) {
+Activity activity("Meeting", QTime(9, 0), QTime(10, 0));
 
+activity.setDescription("Workshop");
+EXPECT_EQ(activity.getDescription(), "Workshop");
+
+activity.setStart(QTime(10, 0));
+EXPECT_EQ(activity.getStart(), QTime(10, 0));
+
+activity.setAnEnd(QTime(11, 0));
+EXPECT_EQ(activity.getAnEnd(), QTime(11, 0));
 }
 
 int main(int argc, char **argv) {
